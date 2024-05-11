341b083b34382fb0fb8fe8e11de58719bbed4027
Dat Nguyen <95663983+devdatnguyen@users.noreply.github.com>
2024-05-11 16:52:13 +0700

    Config DB

---
+++ src/SachHayBlog.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SachHayBlog.Core.Domain.Identity
+{
+    [Table("AppRoles")]
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(250)]
+        public required string DisplayName { get; set; }
+
+    }
+}
