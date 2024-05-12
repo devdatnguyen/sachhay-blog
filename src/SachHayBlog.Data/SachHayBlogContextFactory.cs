359f71ddeee3d6f623ed87ab83731af80dd9f3a2
Dat Nguyen <95663983+devdatnguyen@users.noreply.github.com>
2024-05-12 09:11:54 +0700

    Config migrate database

---
+++ src/SachHayBlog.Data/SachHayBlogContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace SachHayBlog.Data
+{
+    public class SachHayBlogContextFactory : IDesignTimeDbContextFactory<SachHayBlogContext>
+    {
+        public SachHayBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<SachHayBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new SachHayBlogContext(builder.Options);
+        }
+    }
+}
